df2a0eecdf57bd7f338b256e9b03e695070e649d
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-11-09 21:38:19 -0300

    adicionado imagem da matriz valorada, enunciado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import image from './mapa.png';
+import matrizImg from './matrizValorada.png';
 import Graph from './Grafo';
 import { useState } from 'react';
 import matrizValorada from './matriz.json';
@@ -9,6 +10,7 @@ function App() {
   const [cidadeOrigem, setCidadeOrigem] = useState('1');
   const [cidadeDestino, setCidadeDestino] = useState('1');
   const [caminhoMinimo, setCaminhoMinimo] = useState([]);
+  const [toggleImage, setToggleImage] = useState(false);
   const graph = new Graph(matrizValorada);
 
   const handleSubmit = (e) => {
@@ -17,10 +19,29 @@ function App() {
     setCaminhoMinimo(res);
   }
 
+  const handleToggleImage = () => {
+    setToggleImage(!toggleImage);
+  }
+
   return (
     <div className="main center">
+      <h1>AV2 - Programação Linear e Grafos</h1>
+      <div className="enunciado">
+        De forma a reduzir seus custos operacionais, uma empresa de transporte de
+        cargas do estado, deseja oferecer aos motoristas de sua frota um mecanismo que os
+        auxilie a selecionar o caminho de menor custo entre quaisquer duas cidades por ela
+        servidas (as cidades são apresentadas na figura e os custos de transportes entre as duas
+        cidades na <a href="#" onClick={() => { handleToggleImage() }}>matriz valorada</a>). <br />
+        Desenvolver uma aplicação para mostrar o caminho de custo mínimo entre duas
+        cidades quaisquer servidas pela empresa e as cidades pelas que o transporte deverá
+        passar para chegar da cidade origem a cidade destino.
+      </div>
+
+      {toggleImage && <img src={matrizImg} alt="Matriz Valorada" className="image" />}
+
       <img src={image} alt="Mapa" className="image" />
-      <Form onSubmit={handleSubmit} className="center mt-3">
+
+      <Form onSubmit={handleSubmit} className="center">
         <Form.Group controlId="exampleForm.ControlSelect1">
           <Form.Label>Cidade origem:</Form.Label>
           <Form.Control as="select" onChange={e => { setCidadeOrigem(e.target.value) }}>
